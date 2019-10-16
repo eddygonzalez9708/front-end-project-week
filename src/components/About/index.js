c50eb171fb6801e74d0ae850f33d641a712fb8fd
Eddy <edward.gonzalez9708@gmail.com>
2019-10-16 00:15:22 -0400

    Converted About component to a regular function.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const About = () => (
-  <div className='content-sect padding'>
-    <h2>Welcome to the Lambda Notes!</h2>
-    <p>To view, edit, and create notes you must be logged in. If you would like to have an account please feel free to click on the sign up button to register.</p>
-  </div>
-)
+function About() {
+  return (
+    <div className='content-sect padding'>
+      <h2>Welcome to the Lambda Notes!</h2>
+      <p>To view, edit, and create notes you must be logged in. If you would like to have an account please feel free to click on the sign up button to register.</p>
+    </div>
+  )
+}
 
 export default About
