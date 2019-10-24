174041dc6303f242ea137168837ff31a3de2b865
Eddy <edward.gonzalez9708@gmail.com>
2019-10-24 12:00:20 -0400

    Converted the Loading component and the DeleteModal component to regular functional components.

--- src/components/CustomModals/DeleteModal.js
+++ src/components/CustomModals/DeleteModal.js
@@ -7,34 +7,33 @@ import PropTypes from 'prop-types'
 
 import './index.css'
 
-const DeleteModal = ({
-  modal,
-  toggle,
-  removeNote }) => (
-  <Modal
-    className='modal-cont'
-    open={modal}>
-    <Modal.Content>
-      <Modal.Description className='modal-desc'>
-        <h2>Are you sure you want to delete this note?</h2>
-      </Modal.Description>
-      <Modal.Actions className='modal-btn-sect'>
-        <Button
-          as={Link}
-          className='milano-red modal-btn'
-          to='/'
-          onClick={removeNote}>
-          Yes
-        </Button>
-        <Button
-          className='pacific-blue modal-btn'
-          onClick={toggle}>
-          Cancel
-        </Button>
-      </Modal.Actions>
-    </Modal.Content>
-  </Modal>
-)
+function DeleteModal({ modal, toggle, removeNote }) {
+  return (
+    <Modal
+      className = 'modal-cont'
+      open = {modal}>
+      <Modal.Content>
+        <Modal.Description className = 'modal-desc'>
+          <h2>Are you sure you want to delete this note?</h2>
+        </Modal.Description>
+        <Modal.Actions className = 'modal-btn-sect'>
+          <Button
+            as = {Link}
+            className = 'milano-red modal-btn'
+            to = '/'
+            onClick = {removeNote}>
+            Yes
+          </Button>
+          <Button
+            className = 'pacific-blue modal-btn'
+            onClick = {toggle}>
+            Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal.Content>
+    </Modal>
+  )
+}
 
 DeleteModal.propTypes = {
   modal: PropTypes.bool.isRequired,
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Loader } from 'semantic-ui-react'
 
-const Loading = ({ text }) => (
-  <div className = 'loading'>
-    <Loader active inline = 'centered'>{text}</Loader>
-  </div>
-)
+function Loading({ text }) {
+  return (
+    <div className = 'loading'>
+      <Loader active inline = 'centered'>{text}</Loader>
+    </div>
+  )
+}
 
 Loading.propTypes = {
   text: PropTypes.string.isRequired
